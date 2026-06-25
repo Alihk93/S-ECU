d3a740d428390abc2e6dfec6a42f508016d0ae31
Ali.H.K.93 <ali.com.e.93@gmail.com>
2026-06-25 12:56:10 +0300

    update .ignore file

--- Firmware/Smart_ECU/build/CMakeFiles/git-data/grabRef.cmake
+++ Firmware/Smart_ECU/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/ali/MyProjects/S-ECU/Firmware/Smart_ECU/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/ali/MyProjects/S-ECU/.git")
+set(GIT_DIR "/home/ali/MyProjects/S-ECU/Firmware/Smart_ECU/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
